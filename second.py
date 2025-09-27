c6831eafefc8281588c263eca61aa2c29e0f17cc
anvekshasharma265-source <anvekshasharma265@gmail.com>
2025-09-27 15:12:20 +0530

    Add files via upload

---
+++ second.py
@@ -0,0 +1,17 @@
+# Python program to show dynamic typing
+
+# Initially assign an integer
+x = 10
+print("Value of x:", x, "| Type of x:", type(x))
+
+# Reassign a string to the same variable
+x = "Hello, Python!"
+print("Value of x:", x, "| Type of x:", type(x))
+
+# Reassign a list to the same variable
+x = [1, 2, 3]
+print("Value of x:", x, "| Type of x:", type(x))
+
+# Reassign a float to the same variable
+x = 3.14
+print("Value of x:", x, "| Type of x:", type(x))
